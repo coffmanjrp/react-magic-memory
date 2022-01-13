77f8396dd8a21f63a4cfe7e7e42d159f7c9ad8d3
coffmanjrp <coffmanjrp@gmail.com>
2022-01-13 15:00:48 -0800

    Setting up & Shuffling cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
+import { useEffect, useState } from 'react';
 import 'App.css';
 
+const cardImages = [
+  { src: '/img/helmet-1.png' },
+  { src: '/img/potion-1.png' },
+  { src: '/img/ring-1.png' },
+  { src: '/img/scroll-1.png' },
+  { src: '/img/shield-1.png' },
+  { src: '/img/sword-1.png' },
+];
+
+type Cards = {
+  id: number;
+  src: string;
+}[];
+
 function App() {
+  const [cards, setCards] = useState<Cards>([]);
+  const [turns, setTurns] = useState(0);
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
+  console.log(cards, turns);
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button type="button">New Game</button>
+      <button type="button" onClick={shuffleCards}>
+        New Game
+      </button>
     </div>
   );
 }
